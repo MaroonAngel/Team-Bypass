9964ecd07bb920761cab1aa9f9b3578aaf37d29c
Jamie <54751722+MaroonAngel@users.noreply.github.com>
2020-03-12 16:24:53 -0600

    Project setup

---
+++ src/main/java/com/github/maroonangel/TeamBypass.java
@@ -0,0 +1,10 @@
+package com.github.maroonangel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TeamBypass implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
